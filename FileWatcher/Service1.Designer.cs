267f9bb0639ab15a826283054671827757ce234b
Pareek <pareeka2@diebold.com>
2018-01-02 12:02:18 +0530

    First Checkin

---
+++ FileWatcher/Service1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace FileWatcher
+{
+    partial class FileWatcherService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemImageWatcher = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemImageWatcher)).BeginInit();
+            // 
+            // fileSystemImageWatcher
+            // 
+            this.fileSystemImageWatcher.EnableRaisingEvents = true;
+           this.fileSystemImageWatcher.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+            // 
+            // Service1
+            // 
+            this.ServiceName = "FileWatcher";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemImageWatcher)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemImageWatcher;
+    }
+}
---
+++ FileWatcher/Service1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace FileWatcher
+{
+    public partial class FileWatcherService : ServiceBase
+    {
+        string watchPath = ConfigurationManager.AppSettings["WatchPath"];
+        bool eventReceived = false;
+        public FileWatcherService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                fileSystemImageWatcher.Path = watchPath;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            fileSystemImageWatcher.EnableRaisingEvents = false;
+            fileSystemImageWatcher.Dispose();
+        }
+
+        
+        private void fileSystemWatcher_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            if (eventReceived == false)
+            {
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    //Debugger.Break();
+                    //eventLog.Source = "FileWatcher";
+
+                    //eventLog.WriteEntry("Batch file exceution started ...");
+                    //var processInfo = new ProcessStartInfo();
+                    //processInfo.FileName = @"C: \Users\pareeka2\Desktop\New Text Document.bat";
+                    //processInfo.CreateNoWindow = true;
+                    //processInfo.UseShellExecute = false;
+                    //processInfo.RedirectStandardError = true;
+                    //processInfo.RedirectStandardOutput = true;
+                    //var process = Process.Start(processInfo.FileName);
+
+                    //int timeout = 60000;
+
+                    //process.WaitForExit(timeout);
+
+                    //string errorMessage = process.StandardError.ReadToEnd();
+                    //eventLog.WriteEntry("Error message of batch is  : " + errorMessage);
+                    //process.WaitForExit();
+
+                    eventLog.WriteEntry("Directory Changed=> New File Added :" + e.Name);
+                }
+
+                eventReceived = true;
+            }
+            else
+            {
+                eventReceived = false;
+            }
+        }
+    }
+}
